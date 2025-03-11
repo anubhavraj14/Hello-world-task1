3e06c144594302dcb538663cc03321bd2f61c3bf
Anubhav Raj <anubhav_raj@epam.com>
2025-03-11 17:57:03 +0530

    chagnes

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,8 +1,9 @@
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
+    console.log("Received event:", JSON.stringify(event, null, 2));
+
+    return {
         statusCode: 200,
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: "Hello from Lambda" }),
     };
-    return response;
 };
