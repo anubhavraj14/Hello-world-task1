ba5c9cd284773813d35dd7b540632245f34c64c6
Anubhav Raj <anubhav_raj@epam.com>
2025-03-18 16:17:35 +0530

    changed

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,9 +1,6 @@
 exports.handler = async (event) => {
-    console.log("Received event:", JSON.stringify(event, null, 2));
-
     return {
         statusCode: 200,
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ message: "Hello from Lambda" }),
+         message: 'Hello from Lambda' ,
     };
 };
